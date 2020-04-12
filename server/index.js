b2ea87b1fd7860a1d13dd0ca4b34c70a0046807a
Cankun Wang <flykun0620@gmail.com>
2020-04-12 13:58:29 -0400

    rename directory

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import Koa from 'koa'
 import koaRouter from 'koa-router'
 import Cors from '@koa/cors'
+import { ApolloServer, gql } from 'apollo-server-koa'
 import DessoRoute from '@server/routes/desso/router.js'
 //import path from 'path'
 import consola from 'consola'
@@ -13,6 +14,22 @@ const app = new Koa()
 const router = koaRouter()
 
 const port = process.env.PORT
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!'
+  }
+}
+const server = new ApolloServer({ typeDefs, resolvers })
+//server.applyMiddleware({ app })
 
 app.use(async function(ctx, next) {
   let start = new Date()
@@ -28,7 +45,7 @@ app.on('error', function(err, ctx) {
 router.use('/api/desso', DessoRoute.routes())
 app.use(Cors())
 app.use(router.routes())
-
+app.use(server.getMiddleware())
 export default app.listen(port, () => {
   consola.success(`Koa is listening in port: ${port}`)
 })
