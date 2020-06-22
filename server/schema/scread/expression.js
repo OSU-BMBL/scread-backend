d2133e95652d66900b875be62ce0cd4645b89762
Cankun Wang <flykun0620@gmail.com>
2020-06-22 10:40:35 -0400

    update config for hosting on OSUMC

--- server/schema/scread/expression.js
+++ server/schema/scread/expression.js
@@ -1,34 +1,30 @@
 /* jshint indent: 2 */
 
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define(
-    'expression',
-    {
-      id: {
-        type: DataTypes.INTEGER(11),
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      data_id: {
-        type: DataTypes.CHAR(50),
-        allowNull: true
-      },
-      gene: {
-        type: DataTypes.STRING(50),
-        allowNull: true
-      },
-      cell_name: {
-        type: DataTypes.STRING(50),
-        allowNull: true
-      },
-      expression: {
-        type: DataTypes.INTEGER(11),
-        allowNull: true
-      }
+  return sequelize.define('expression', {
+    id: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
-    {
-      tableName: 'expression'
+    data_id: {
+      type: DataTypes.CHAR(50),
+      allowNull: true
+    },
+    gene: {
+      type: DataTypes.STRING(50),
+      allowNull: true
+    },
+    cell_name: {
+      type: DataTypes.STRING(50),
+      allowNull: true
+    },
+    expression: {
+      type: DataTypes.INTEGER(11),
+      allowNull: true
     }
-  )
-}
+  }, {
+    tableName: 'expression'
+  });
+};
